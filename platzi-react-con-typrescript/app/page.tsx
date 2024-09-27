65de2fc294e0cd7ede03fe989e97ccef67606552
adan_due <ing.adanduenas@gmail.com>
2024-09-27 12:28:21 -0600

    State con tipos personalizados

--- platzi-react-con-typrescript/app/page.tsx
+++ platzi-react-con-typrescript/app/page.tsx
@@ -3,12 +3,17 @@ import { RandomFox } from "./components/RandomFox";
 
 const random = () : number => Math.floor(Math.random() * 123) + 1;
 
+//generate simple unique id
+const generateId = () => Math.random().toString(36).substr(2, 9)
+
+type ImageItem = { id: string, url: string }
+
 export default function Home() {
-  const [images, setImages] = useState<Array<string>>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>>([
+    { id:generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    { id:generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    { id:generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    { id:generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
   ]);
 
   return (
@@ -16,9 +21,9 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Hello Platzi!
       </h1>
-      {images.map((image, index) => (
-          <div className="p-4" key={index}>
-            <RandomFox image={image} />
+      {images.map(({id, url}) => (
+          <div className="p-4" key={id}>
+            <RandomFox image={url} />
           </div>
       ))}
     </>
