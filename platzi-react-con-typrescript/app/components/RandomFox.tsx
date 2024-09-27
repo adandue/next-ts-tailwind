6346a7c6247030eb40ae4926929a0d6cf6ffa05b
adan_due <ing.adanduenas@gmail.com>
2024-09-27 11:49:04 -0600

    El objeto props y children

--- platzi-react-con-typrescript/app/components/RandomFox.tsx
+++ platzi-react-con-typrescript/app/components/RandomFox.tsx
@@ -1,8 +1,6 @@
 //generate a random function between 1 and 123
-const random = () : number => Math.floor(Math.random() * 123) + 1;
+type Props = { image:string}
 
-export const RandomFox = (): JSX.Element => {
-    const image : string = `https://randomfox.ca/images/${random()}.jpg`;
-    
+export const RandomFox = ({ image }: Props): JSX.Element => {
     return <img width={320} height="auto" className="rounded-xl" src={image} /> 
 }
--- platzi-react-con-typrescript/app/page.tsx
+++ platzi-react-con-typrescript/app/page.tsx
@@ -1,12 +1,14 @@
 import { RandomFox } from "./components/RandomFox";
 
+const random = () : number => Math.floor(Math.random() * 123) + 1;
+
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Hello Platzi!
       </h1>
-      <RandomFox />
+      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
     </>
   );
 }
