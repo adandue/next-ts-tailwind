c5bf9846f98dd0b13af412c5cf09427c2667e670
adan_due <ing.adanduenas@gmail.com>
2024-09-27 12:15:24 -0600

    State con tipos primitivos

--- platzi-react-con-typrescript/app/page.tsx
+++ platzi-react-con-typrescript/app/page.tsx
@@ -1,14 +1,26 @@
+'use client'; import { useState } from "react";
 import { RandomFox } from "./components/RandomFox";
 
 const random = () : number => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Hello Platzi!
       </h1>
-      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+      {images.map((image, index) => (
+          <div className="p-4" key={index}>
+            <RandomFox image={image} />
+          </div>
+      ))}
     </>
   );
 }
