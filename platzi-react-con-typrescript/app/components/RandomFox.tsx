4d355cb1556c51c69f0904be995def1009b349f2
adan_due <ing.adanduenas@gmail.com>
2024-09-30 13:03:09 -0600

    Tipos de referencias y observadores

--- platzi-react-con-typrescript/app/components/RandomFox.tsx
+++ platzi-react-con-typrescript/app/components/RandomFox.tsx
@@ -1,6 +1,11 @@
 //generate a random function between 1 and 123
+import { useRef } from 'react'
 type Props = { image:string}
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
-    return <img width={320} height="auto" className="rounded-xl" src={image} /> 
+
+    const node = useRef<HTMLImageElement>(null)
+    node.current?.scroll
+
+    return <img ref={node} width={320} height="auto" className="rounded-xl" src={image} /> 
 }
